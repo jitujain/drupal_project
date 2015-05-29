11cb852768f53794e71b7e829ee34ceabe3148d9
jitujain <jitendra.jain@innoraft.com>
2015-05-29 20:31:27 +0530

    Apply jquert travesing refs #9923

--- sites/all/modules/feedback/feedback.js
+++ sites/all/modules/feedback/feedback.js
@@ -7,8 +7,46 @@
   });
 
   $(document).ready(function(){
-    $("#k1").click(function(){
-      $no = $("#")
+    $("span.knowledge").click(function(){
+      $len = $(this).attr("class");
+      $s = $len.split(" ");
+      var no = $s[3];
+     // $("span").eq(i).addClass("redstar");
+      var i;
+      // for(i=1 ; i=<no ;i++) {
+      //   $("span")."whitestar.spanspace.knowledge".i.addClass("redstar");
+
+      // }
+      if($(this).attr("class") == "whitestar spanspace knowledge 4")
+      {
+        alert("hello");
+      }
+
+    });
+  });
+
+$(document).ready(function(){
+    $("whitspace").click(function(){
+      if($(this).attr("class").match("abc") && $(this).attr("class").match("xyz") )
+      {
+        alert("hello");
+      }
+
+
+  $(document).ready(function(){
+    $("span.attitude").click(function(){
+      $len = $(this).attr("class");
+      $s = $len.split(" ");
+      alert($s[3]);
+
+    });
+  });
+  $(document).ready(function(){
+    $("span.friendliness").click(function(){
+      $len = $(this).attr("class");
+      $s = $len.split(" ");
+      alert($s[3]);
+
     });
   });
 
