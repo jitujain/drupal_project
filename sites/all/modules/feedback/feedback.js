0f5239e85d9439a92b52c92101f5b7fa00077e5f
jitujain <jitendra.jain@innoraft.com>
2015-05-29 12:40:24 +0530

    All functionality work except one
    Values are not set in the textfields
    refs #9923

--- sites/all/modules/feedback/feedback.js
+++ sites/all/modules/feedback/feedback.js
@@ -1,8 +1,15 @@
 (function ($) {
+
+  $(document).ready(function(){
+    $("#edit-choice-0").click(function(){
+      $("input:text").val('');
+    });
+  });
+
   Drupal.behaviors.feedback = {
     attach: function (context, settings) {
       // Your Javascript code goes here
-      // alert(Drupal.settings.feedback.tax_rate);
+    // alert(Drupal.settings.feedback.tax_rate);
 
     }
   };
